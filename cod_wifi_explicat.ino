6c3a47a84c728d84076adbd97a5324fe0fb0fddf
alex19dragu <99834253+alex19dragu@users.noreply.github.com>
2024-07-28 12:23:33 +0300

    Adaugat butoane valva si ventilator

--- cod_wifi_explicat.ino
+++ cod_wifi_explicat.ino
@@ -21,8 +21,44 @@ char pass[] =  "xxxxx";
 #define wifiLed   LED_BUILTIN
 //declarare led de pe placuta Wi-Fi
 
+bool stareValva = 0;
+bool stareFan1 = 0;
+bool stareFan2 = 0;
+//declarare stari valva si ventilator
+
+#define pinValvaVirtual V19
+#define pinFanVirtual1 V29
+#define pinFanVirtual2 V7
+//declarare pini virtuali valva si ventilator
+
+#define pinValvaEsp D3
+#define pinFanEsp1 D4
+#define pinFanEsp2 D5
+//declarare pini digitali valva si ventilator
+
 BlynkTimer timer;
 //declarare unui timer cu ajutorul caruia verificam daca serverul Blynk este conectat
+
+BLYNK_WRITE(pinValvaVirtual)
+{      
+  stareValva = param.asInt();
+  digitalWrite(pinValvaEsp, stareValva);    
+  //schimbare stare valva
+}
+
+BLYNK_WRITE(pinFanVirtual1)
+{      
+  stareFan1 = param.asInt();
+  digitalWrite(pinFanEsp1, stareFan1);    
+  //schimbare stare pin 1 ventilator
+}
+
+BLYNK_WRITE(pinFanVirtual2)
+{      
+  stareFan2 = param.asInt();
+  digitalWrite(pinFanEsp2, stareFan2);
+  //schimbare stare pin 2 ventilator
+}
 
 void checkBlynkStatus() 
 {
@@ -42,20 +78,25 @@ void checkBlynkStatus()
 
 void setup()
 {     
-      Serial.begin(115200);
-      mySerial.begin(9600);
-      //initializare placuta Esp8266
+  Serial.begin(115200);
+  mySerial.begin(9600);
+  //initializare placuta Esp8266
            
-      pinMode(wifiLed, OUTPUT);
-      //initializare led
-  
-      Blynk.begin(auth, ssid, pass);
-      //initializare server Blynk
+  pinMode(wifiLed, OUTPUT);
+  //initializare led
 
-      timer.setInterval(2000L, checkBlynkStatus); 
-      //la fiecare 2 secunde verificam daca serverul este conectat
+  pinMode(pinValvaEsp, OUTPUT);
+  pinMode(pinFanEsp1, OUTPUT);
+  pinMode(pinFanEsp2, OUTPUT);    
+  //initializare pini valva si ventilator
+  
+  Blynk.begin(auth, ssid, pass);
+  //initializare server Blynk
+
+  timer.setInterval(2000L, checkBlynkStatus); 
+  //la fiecare 2 secunde verificam daca serverul este conectat
       
-      delay(1000);
+  delay(1000);
 }
 
 void loop()
